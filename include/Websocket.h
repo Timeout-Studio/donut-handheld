0fcf3664ae178d574a622d3588c668237c4fc6a6
Aaron Low <aaronlow1220@gmail.com>
2025-02-17 23:11:21 +0800

    feat: Websocket init

--- include/Websocket.h
+++ include/Websocket.h
@@ -1,7 +1,13 @@
 #pragma once
 
-class Websocket
+class Dn_Websocket
 {
 private:
+    char *ssid;
+    char *password;
 public:
+    void init(char *ssid, char *password);
+    void send(char *message);
+    void receive();
+    void close();
 };
--- src/Websocket.cpp
+++ src/Websocket.cpp
@@ -1,1 +1,8 @@
+#include "Websocket.h"
 #include <Websocket.h>
+
+void Dn_Websocket::init(char* ssid, char* password)
+{
+    this->ssid = ssid;
+    this->password = password;
+};
